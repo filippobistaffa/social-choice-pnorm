5c4b297c1657f9e482dacf1455fe453183906caa
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2020-10-22 14:38:10 +0200

    Updated code with p=1, p=inf, and upper triangular

--- single_norm.py
+++ single_norm.py
@@ -1,3 +1,4 @@
+from docplex.mp.model import Model
 import argparse as ap
 import numpy as np
 np.set_printoptions(edgeitems=1000, linewidth=1000, suppress=True, precision=5)
@@ -9,20 +10,30 @@ parser.add_argument('-p', type=int, default=2, help='p')
 parser.add_argument('-e', type=float, default=1e-10, help='e')
 parser.add_argument('-w', type=str, default='w.csv', help='CSV file with weights')
 parser.add_argument('-b', type=str, default='b.csv', help='CSV file with b vector')
+parser.add_argument('-u', help='optimize only upper-triangular', action='store_true')
 args = parser.parse_args()
 
 p = args.p
 n = args.n
 m = args.m
-m2 = m*m
 
-b = np.genfromtxt(args.b)
+if args.u:
+    idx = []
+    for i in range(n):
+        for j in range(m):
+            for k in range(j):
+                idx.append(k + m * j + m * m * i)
+    l = int(m * (m - 1) / 2)
+    b = np.genfromtxt(args.b)[idx]
+else:
+    b = np.genfromtxt(args.b)
+    l = m * m
+
 w = np.genfromtxt(args.w)
-
-w = np.repeat(w, m2)
+w = np.repeat(w, l)
 b = np.multiply(b, w)
 
-A = np.tile(np.identity(m2), (n, 1))
+A = np.tile(np.identity(l), (n, 1))
 A = np.multiply(A, w.reshape(-1, 1))
 
 # constraints needed for pIRLS (empty)
@@ -34,21 +45,89 @@ d = np.zeros_like(b)
 #print('b =')
 #print(b.reshape(-1, 1))
 
-x_n, res, rank, a = np.linalg.lstsq(A, b, rcond=None)
+if p == 2:
+    cons, res, rank, a = np.linalg.lstsq(A, b, rcond=None)
+    print('NUMPY L2: x =')
+    if args.u:
+        tmat = np.zeros((m * m,))
+        tmat[idx[:l]] = cons
+        print(tmat.reshape(m, m).T)
+    else:
+        print(cons.reshape((m, m)).T)
+elif p == 1:
+    model = Model("Sum of absolute residuals approximation")
+    # create variables
+    t = model.continuous_var_list(len(b))
+    x = model.continuous_var_list(l)
+    # create constraints
+    I = range(len(b))   # size of b
+    J = range(l)        # size of x
+    for i in I:
+        model.add_constraint(model.sum(A[i,j] * x[j] for j in J) - b[i] >= -t[i])
+        model.add_constraint(model.sum(A[i,j] * x[j] for j in J) - b[i] <= t[i])
+    model.minimize(model.sum(t))
+    # optimize model
+    solution = model.solve()
+    cons = np.zeros((l,))
+    for j in J:
+        cons[j] = solution.get_value(x[j])
+    print('CPLEX L1: x =')
+    if args.u:
+        tmat = np.zeros((m * m,))
+        tmat[idx[:l]] = cons
+        print(tmat.reshape(m, m).T)
+    else:
+        print(cons.reshape((m, m)).T)
+elif p == -1:
+    model = Model("Chebyshev approximation")
+    # create variables
+    t = model.continuous_var()
+    x = model.continuous_var_list(l)
+    # create constraints
+    I = range(len(b))   # size of b
+    J = range(l)        # size of x
+    for i in I:
+        model.add_constraint(model.sum(A[i,j] * x[j] for j in J) - b[i] >= -t)
+        model.add_constraint(model.sum(A[i,j] * x[j] for j in J) - b[i] <= t)
+    model.minimize(t)
+    # optimize model
+    solution = model.solve()
+    cons = np.zeros((l,))
+    for j in J:
+        cons[j] = solution.get_value(x[j])
+    print('CPLEX L∞: x =')
+    if args.u:
+        tmat = np.zeros((m * m,))
+        tmat[idx[:l]] = cons
+        print(tmat.reshape(m, m).T)
+    else:
+        print(cons.reshape((m, m)).T)
+    # override solution with the one from Omega
+    #cons = np.array([5,1,5,1.4,5,5,1,3,7,3])
+    #if args.u:
+    #    tmat = np.zeros((m * m,))
+    #    tmat[idx[:l]] = cons
+    #    print(tmat.reshape(m, m).T)
+    #else:
+    #    print(cons.reshape((m, m)))
+else:
+    print('Initialising Julia...')
+    from julia.api import Julia
+    jl = Julia(compiled_modules=False)
+    from julia import Main
+    Main.include('pIRLS/IRLS-pNorm.jl')
+    cons, it = Main.pNorm(args.e, A, b.reshape(-1, 1), p, C, d.reshape(-1, 1))
+    print('JULIA L{}: x ='.format(p))
+    if args.u:
+        tmat = np.zeros((m * m,))
+        tmat[idx[:l]] = cons
+        print(tmat.reshape(m, m).T)
+    else:
+        print(cons.reshape((m, m)).T)
 
-print('NUMPY L2: x =')
-print(x_n.reshape((m, m)))
-
-print('Initialising Julia...')
-from julia.api import Julia
-jl = Julia(compiled_modules=False)
-from julia import Main
-Main.include('pIRLS/IRLS-pNorm.jl')
-x_j, it = Main.pNorm(args.e, A, b.reshape(-1, 1), p, C, d.reshape(-1, 1))
-print('JULIA L{}: x ='.format(p))
-print(x_j.reshape((m, m)))
-
-r = np.abs(A @ x_j - b)
+r = np.abs(A @ cons - b)
+#print('Residuals =', r)
+print('Max residual =', np.max(r))
 h, b = np.histogram(r, bins=np.arange(10))
-print('Residuals =')
+print('Residuals distribution =')
 print(np.vstack((h, b[:len(h)], np.roll(b, -1)[:len(h)])))
