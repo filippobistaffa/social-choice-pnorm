8bf49ffb14bc6782bcd08b4b49238eab5d0709d9
Filippo Bistaffa <filippo.bistaffa@iiia.csic.es>
2020-11-05 19:50:32 +0100

    Cumulative

--- generator.py
+++ generator.py
@@ -22,26 +22,46 @@ def generate_unstructured(low, high, number_of_principles):
 def generate_gaussian(mu, sigma, n):
     return np.random.normal(mu, sigma, n)
 
+def append(cumulative, directory, distribution):
+    if directory in cumulative:
+        cumulative[directory] = np.append(cumulative[directory], distribution)
+    else:
+        cumulative[directory] = distribution
+
 if __name__ == '__main__':
     os.makedirs(b_dir, exist_ok=True)
+    cumulative = {}
     for i in range(reps):
         # polarised
         for j in range(1, 7):
             dist = generate_polarised(1.05, 0.05, 2.15, 0.05, j, 7 - j)
-            p, l = np.unique(np.around(dist, precision), return_counts=True)
+            dist = np.around(dist, precision)
+            dist = np.clip(dist, 1, 3)
+            p, l = np.unique(dist, return_counts=True)
             p_dir_t = p_dir + '_{}_{}'.format(j, 7 - j)
             os.makedirs(p_dir_t, exist_ok=True)
             np.savetxt(os.path.join(p_dir_t, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
             np.savetxt(os.path.join(p_dir_t, 'l_{}.csv'.format(i)), l, fmt='%d')
+            append(cumulative, p_dir_t, dist)
         # unstructured
         dist = generate_unstructured(1, 2.9, 7)
-        p, l = np.unique(np.around(dist, precision), return_counts=True)
+        dist = np.around(dist, precision)
+        dist = np.clip(dist, 1, 3)
+        p, l = np.unique(dist, return_counts=True)
         os.makedirs(u_dir, exist_ok=True)
         np.savetxt(os.path.join(u_dir, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
         np.savetxt(os.path.join(u_dir, 'l_{}.csv'.format(i)), l, fmt='%d')
+        append(cumulative, u_dir, dist)
         # gaussian
         dist = generate_gaussian(1.4, 0.05, 7)
-        p, l = np.unique(np.around(dist, precision), return_counts=True)
+        dist = np.around(dist, precision)
+        dist = np.clip(dist, 1, 3)
+        p, l = np.unique(dist, return_counts=True)
         os.makedirs(g_dir, exist_ok=True)
         np.savetxt(os.path.join(g_dir, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
         np.savetxt(os.path.join(g_dir, 'l_{}.csv'.format(i)), l, fmt='%d')
+        append(cumulative, g_dir, dist)
+    for directory, distribution in cumulative.items():
+        p, l = np.unique(distribution, return_counts=True)
+        np.savetxt(os.path.join(directory, 'cumul.csv'), np.vstack((p, l)).T,
+            fmt='%.{}f'.format(precision), delimiter=',')
