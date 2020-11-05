a8986b00476bff59dabe522da4f52acd61dbfd14
Filippo Bistaffa <filippo.bistaffa@iiia.csic.es>
2020-11-05 12:39:13 +0100

    Updated generator

--- generator.py
+++ generator.py
@@ -1,8 +1,11 @@
 import numpy as np
 import os
 
+seed = 0
 reps = 10
 precision = 2
+
+np.random.seed(seed)
 b_dir = 'distributions'
 p_dir = os.path.join(b_dir, 'polarised')
 u_dir = os.path.join(b_dir, 'unstructured')
@@ -20,20 +23,25 @@ def generate_gaussian(mu, sigma, n):
     return np.random.normal(mu, sigma, n)
 
 if __name__ == '__main__':
-    for d in [b_dir, p_dir, u_dir, g_dir]:
-        os.makedirs(d, exist_ok=True)
+    os.makedirs(b_dir, exist_ok=True)
     for i in range(reps):
-        dist = generate_polarised(1.2, 0.05, 2.15,0.05, 4, 3)
-        p, l = np.unique(np.around(dist, precision), return_counts=True)
-        np.savetxt(os.path.join(p_dir, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
-        np.savetxt(os.path.join(p_dir, 'l_{}.csv'.format(i)), l, fmt='%d')
-        #
+        # polarised
+        for j in range(1, 7):
+            dist = generate_polarised(1.2, 0.05, 2.15,0.05, j, 7 - j)
+            p, l = np.unique(np.around(dist, precision), return_counts=True)
+            p_dir_t = p_dir + '_{}_{}'.format(j, 7 - j)
+            os.makedirs(p_dir_t, exist_ok=True)
+            np.savetxt(os.path.join(p_dir_t, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
+            np.savetxt(os.path.join(p_dir_t, 'l_{}.csv'.format(i)), l, fmt='%d')
+        # unstructured
         dist = generate_unstructured(1, 2.9, 7)
         p, l = np.unique(np.around(dist, precision), return_counts=True)
+        os.makedirs(u_dir, exist_ok=True)
         np.savetxt(os.path.join(u_dir, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
         np.savetxt(os.path.join(u_dir, 'l_{}.csv'.format(i)), l, fmt='%d')
-        #
+        # gaussian
         dist = generate_gaussian(1.4, 0.05, 7)
         p, l = np.unique(np.around(dist, precision), return_counts=True)
+        os.makedirs(g_dir, exist_ok=True)
         np.savetxt(os.path.join(g_dir, 'p_{}.csv'.format(i)), p, fmt='%.{}f'.format(precision))
         np.savetxt(os.path.join(g_dir, 'l_{}.csv'.format(i)), l, fmt='%d')
