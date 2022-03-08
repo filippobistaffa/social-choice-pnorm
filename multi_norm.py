9e34421cb662fc6d93c9368104d50d141a6c087c
Filippo Bistaffa <filippo.bistaffa@iiia.csic.es>
2022-03-08 10:08:10 +0100

    Updated multi-norm code

--- multi_norm.py
+++ multi_norm.py
@@ -1,80 +1,7 @@
-import warnings
 import argparse as ap
+import cvxpy as cp
 import numpy as np
 np.set_printoptions(edgeitems=1000, linewidth=1000, suppress=True, precision=4)
-
-def residue_norm(e_list=None):
-    residue = 0.0
-    for e in e_list:
-        residue = residue + np.linalg.norm(e)
-    return residue
-
-def solve(A_list=None, b_list=None, lambda_list=None, p_list=None, max_iter=1000000000, tol=1.0e-8):
-    """
-    Solves a general norm minimization problem
-        minimize_x \sum_k \lambda_k * ||A_k x - b_k||_{p_k}^{p_k}
-
-    :param A_list: List of matrices A_k \in \mathbb{R}^{m_k \times n}
-    :param b_list: List of vectors b_k \in \mathbb{R}^{m_k}
-    :param lambda_list: List of weighting scalar \lambda_k \in \mathbb{R}
-    :param p_list: List of norm indicators p_k \ in \mathbb{R}
-    :param max_iter: Maximum number of iterations
-    :param tol: Tolerance
-    :return: x \in \mathbb{R}^n
-    :return: residue norm
-    :return: number of iterations spent for computation
-    """
-
-    alpha = 0    #1.0e-8   # small value for regularizing the weighted least squares
-    eps = 1.0e-8    # small value for avoiding zero-division in weight update
-
-    if A_list is None or b_list is None or lambda_list is None or p_list is None:
-        raise ValueError("Required argument is empty")
-    K = len(A_list)
-    if K != len(b_list) or K != len(lambda_list) or K != len(p_list):
-        raise ValueError("Inconsistent arguments")
-    if any(lambda_list[a] < 0 for a in range(len(lambda_list))):
-        raise ValueError("lambda needs to be all positive")
-    if any(p_list[a] < 0 for a in range(len(p_list))):
-        raise ValueError("p needs to be all positive")
-    for index in range(len(b_list)):
-        if b_list[index].ndim == 1:
-            b_list[index] = b_list[index][:, np.newaxis]
-
-    n = A_list[0].shape[1]    # domain dim
-    x_old = np.ones((n, 1))
-    In = np.identity(n)
-    w_list = []
-    e_list = []
-    for k in range(K):
-        w_list.append(np.identity(A_list[k].shape[0]))
-        e_list.append(np.zeros((A_list[k].shape[0], 1)))
-
-    ite = 0
-    while ite < max_iter:
-        ite = ite + 1
-        C = alpha * In    # n \times n matrix
-        d = np.zeros((n, 1))    # n-vector
-        # Create a normal equation of the problem
-        for k in range(K):
-            C = C + p_list[k] * lambda_list[k] * np.dot(np.dot(A_list[k].T, w_list[k]), A_list[k])
-            d = d + p_list[k] * lambda_list[k] * np.dot(np.dot(A_list[k].T, w_list[k]), b_list[k])
-        x = np.linalg.solve(C, d)
-        for k in range(K):
-            e_list[k] = b_list[k] - A_list[k].dot(x)
-        # stopping criteria
-        if np.linalg.norm(x - x_old) < tol:
-            return x.ravel(), residue_norm(e_list), ite
-        else:
-            #print(x[0], x[1])
-            x_old = x
-        # update weights
-        for k in range(K):
-            w_list[k] = np.diag(
-               np.asarray(1.0 / np.maximum(np.power(np.fabs(e_list[k]), 2.0 - p_list[k]), eps))[:, 0])
-
-    warnings.warn("Exceeded the maximum number of iterations")
-    return x.ravel(), residue_norm(e_list), ite
 
 def print_consensus(cons):
     print('Rs =')
@@ -84,6 +11,25 @@ def print_consensus(cons):
         print(tmat.reshape(m, m).T)
     else:
         print(cons.reshape((m, m)).T)
+
+def Lp_norm(A, b, p):
+    x = cp.Variable(l)
+    cost = cp.pnorm(A @ x - b, p)
+    prob = cp.Problem(cp.Minimize(cost))
+    prob.solve(solver='CPLEX', verbose=False)
+    return prob.value
+
+def mLp(A, b, ps, λs):
+    wps = [λ / Lp_norm(A, b, p) for λ, p in zip(λs, ps)]
+    #wps = [λ for λ, p in zip(λs, ps)]
+    #print(list(zip(wps, ps)))
+    x = cp.Variable(l)
+    cost = cp.sum([wp * cp.pnorm(A @ x - b, p) for wp, p in zip(wps, ps)])
+    prob = cp.Problem(cp.Minimize(cost))
+    prob.solve(solver='CPLEX', verbose=False, cplex_params={})
+    res = np.abs(A @ x.value - b)
+    #print([wp * np.linalg.norm(res, p) for wp, p in zip(wps, ps)])
+    return x.value, res, prob.value / sum(wps)
 
 if __name__ == '__main__':
     parser = ap.ArgumentParser()
@@ -100,12 +46,9 @@ if __name__ == '__main__':
 
     n = args.n
     m = args.m
-
-    w = np.genfromtxt(args.w)
-    p = np.atleast_1d(np.genfromtxt(args.p))
-
-    if len(p) < 2:
-        raise ValueError('Specify at least 2 norm values')
+    ps = np.atleast_1d(np.genfromtxt(args.p))
+    ps = np.where(ps == -1, np.inf, ps)
+    λs = np.atleast_1d(np.genfromtxt(args.l))
 
     if args.u:
         idx = []
@@ -119,6 +62,7 @@ if __name__ == '__main__':
         b = np.genfromtxt(args.b)
         l = m * m
 
+    w = np.genfromtxt(args.w)
     w = np.repeat(w, l)
     b = np.multiply(b, w)
 
@@ -130,16 +74,14 @@ if __name__ == '__main__':
         print(A)
         print('b =')
         print(b.reshape(-1, 1))
-        print('p =', p)
+        print('p =', ps)
+        print('λ =', λs)
 
-    cons, u, it = solve(A_list=[A] * len(p), b_list=[b] * len(p), p_list=p,
-                        lambda_list=np.atleast_1d(np.genfromtxt(args.l))[:len(p)])
+    cons, r, u = mLp(A, b, ps, λs)
     print_consensus(cons)
-    print('U{} = {:.3f}'.format(p, u))
-    print()
-    r = np.abs(A @ cons - b)
+    print('U{} = {:.4f}\n'.format(ps, u))
     #print('Residuals =', r)
-    print('Max residual = {:.3f}'.format(np.max(r)))
+    print('Max residual = {:.4f}'.format(np.max(r)))
     h, b = np.histogram(r, bins=np.arange(10))
     print('Residuals distribution =')
     print(np.vstack((h, b[:len(h)], np.roll(b, -1)[:len(h)])))
